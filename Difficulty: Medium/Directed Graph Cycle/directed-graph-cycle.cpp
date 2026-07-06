b46439cb37d5d6550b1529324e8b7415c896ebca
arryanngupta <160673725+arryanngupta@users.noreply.github.com>
2026-07-06 10:58:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -2,34 +2,32 @@ class Solution {
   public:
   
     vector<vector<int>> adjList;
-    vector<int> visited,path;
+    vector<int> ans;
     
-    bool dfs(int node){
-        visited[node] = 1;
-        path[node] = 1;
-        for(auto it: adjList[node]){
-            if(!visited[it]){
-                if(dfs(it)) return true;
-            }
-            else if(path[it]) return true;
-        }
-        path[node]=0;
-        return false;
-    }
-  
     bool isCyclic(int V, vector<vector<int>> &edges) {
         // code here
-        visited.resize(V,0);
-        path.resize(V,0);
         adjList.resize(V);
+        vector<int> inDeg(V,0);
         for(auto it: edges){
             adjList[it[0]].push_back(it[1]);
+            inDeg[it[1]]++;
         }
+        queue<int> q;
         for(int i = 0; i<V; i++){
-            if(!visited[i]){
-                if(dfs(i)) return true;
+            if(inDeg[i]==0){
+                q.push(i);
+            }
+        }   
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it: adjList[node]){
+                inDeg[it]--;
+                if(inDeg[it]==0) q.push(it);
             }
         }
-        return false;
+        return !(ans.size()==V);
+        
     }
 };
