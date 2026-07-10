9da1e3fe6c18166c1dd6a2aef3497bdc282bc34c
arryanngupta <160673725+arryanngupta@users.noreply.github.com>
2026-07-10 13:11:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Common Subsequence/longest-common-subsequence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+  
+    vector<vector<int>> dp;
+  
+    int recFind(int i,int j,string &s1, string &s2,int n,int m){
+        if(i==n || j==m) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int take = 0,notTake = 0;
+        if(s1[i]==s2[j]){
+            take = 1+recFind(i+1,j+1,s1,s2,n,m);
+        }
+        notTake = recFind(i+1,j,s1,s2,n,m);
+        notTake = max(notTake,recFind(i,j+1,s1,s2,n,m));
+        return dp[i][j]=max(take,notTake);
+    }
+  
+    int lcs(string &s1, string &s2) {
+        // code here
+        int n = s1.size(),m = s2.size();
+        dp.resize(n,vector<int> (m,-1));
+        return recFind(0,0,s1,s2,n,m);
+    }
+    
+};
