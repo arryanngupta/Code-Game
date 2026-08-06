1617d2c13ad5b7f803080d197d31cf33066b4864
arryanngupta <160673725+arryanngupta@users.noreply.github.com>
2026-08-06 21:01:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Detect Loop in linked list/detect-loop-in-linked-list.cpp
@@ -0,0 +1,26 @@
+/*
+class Node {
+   public:
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+} */
+
+class Solution {
+  public:
+    bool detectLoop(Node* head) {
+        // code here
+        Node* fast = head;
+        Node* slow = head;
+        while(fast && fast->next){
+            fast = fast->next->next;
+            slow = slow->next;
+            if(fast==slow) return true;
+        }
+        return false;
+    }
+};
