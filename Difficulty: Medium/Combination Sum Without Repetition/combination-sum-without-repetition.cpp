ebcd6931e0a2e9131df7e056eb6281d81a8587af
arryanngupta <160673725+arryanngupta@users.noreply.github.com>
2026-08-12 03:35:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Combination Sum Without Repetition/combination-sum-without-repetition.cpp
@@ -0,0 +1,30 @@
+class Solution {
+  public:
+  
+    set<vector<int>> ans;
+    
+    void recFind(int idx,vector<int> a,vector<int> &arr, int target,int n){
+        if(target==0){
+            sort(a.begin(),a.end());
+            ans.insert(a);
+            return ;
+        }
+        if(idx>=n){
+            return ;
+        }
+        if(arr[idx]<=target){
+            a.push_back(arr[idx]);
+            recFind(idx+1,a,arr,target-arr[idx],n);
+            a.pop_back();
+        }
+        recFind(idx+1,a,arr,target,n);
+    }   
+  
+    vector<vector<int>> uniqueCombinations(vector<int> &arr, int target) {
+        // code here
+        int n = arr.size();
+        vector<int> a;
+        recFind(0,a,arr,target,n);
+        return vector<vector<int>> (ans.begin(),ans.end());
+    }
+};
