6f41d883b48894f6e4591a93b4dacc4719605f65
arryanngupta <160673725+arryanngupta@users.noreply.github.com>
2026-08-19 16:24:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge two BST 's/merge-two-bst-s.cpp
+++ Difficulty: Medium/Merge two BST 's/merge-two-bst-s.cpp
@@ -1,45 +1,45 @@
-/*
+/* Structure of a Binary Search Tree node
 class Node {
-  public:
+public:
     int data;
-    Node *left;
-    Node *right;
+    Node* left;
+    Node* right;
 
     Node(int val) {
         data = val;
-        left = right = NULL;
+        left = nullptr;
+        right = nullptr;
     }
-};
-*/
+}; */
 
 class Solution {
   public:
   
-    void pushLeft(Node* root,stack<Node*> &s){
-        while(root){
-            s.push(root);
-            root = root->left;
+    void pushLeft(Node* node,stack<Node*> &st){
+        while(node){
+            st.push(node);
+            node = node->left;
         }
     }
-  
-    vector<int> merge(Node *root1, Node *root2) {
+    
+    vector<int> merge(Node *r1, Node *r2) {
         // code here
-        vector<int> ans;
         stack<Node*> st1;
         stack<Node*> st2;
-        pushLeft(root1,st1);
-        pushLeft(root2,st2);
+        pushLeft(r1,st1);
+        pushLeft(r2,st2);
+        vector<int> ans;
         while(!st1.empty() || !st2.empty()){
             if(st2.empty() || (!st1.empty() && st1.top()->data<=st2.top()->data)){
                 Node* node = st1.top();
-                st1.pop();
                 ans.push_back(node->data);
+                st1.pop();
                 pushLeft(node->right,st1);
             }
             else{
                 Node* node = st2.top();
-                st2.pop();
                 ans.push_back(node->data);
+                st2.pop();
                 pushLeft(node->right,st2);
             }
         }
