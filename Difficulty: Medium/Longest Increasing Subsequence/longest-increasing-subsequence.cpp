46aca565608a43c1e8511d7302cd75dc4d40017b
arryanngupta <160673725+arryanngupta@users.noreply.github.com>
2026-09-03 13:47:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.cpp
+++ Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.cpp
@@ -1,23 +1,15 @@
 class Solution {
   public:
   
-    vector<vector<int>> dp;
-  
-    int recFind(int idx,int prev,vector<int>& arr,int n){
-        if(idx>=n) return 0;
-        if(dp[idx][prev+1]!=-1) return dp[idx][prev+1];
-        int take = 0,notTake = 0;
-        if(prev==-1 || arr[idx]>arr[prev]){
-            take = 1+recFind(idx+1,idx,arr,n);
-        }
-        notTake = recFind(idx+1,prev,arr,n);
-        return dp[idx][prev+1]=max(take,notTake);
-    }
-  
     int lis(vector<int>& arr) {
         // code here
         int n = arr.size();
-        dp.resize(n,vector<int> (n+1,-1));
-        return recFind(0,-1,arr,n);
+        vector<int> ans;
+        for(int i = 0; i<n; i++){
+            int lb = lower_bound(ans.begin(),ans.end(),arr[i])-ans.begin();
+            if(lb==ans.size()) ans.push_back(arr[i]);
+            else ans[lb] = arr[i];
+        }
+        return ans.size();
     }
 };
